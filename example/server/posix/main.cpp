baf8d0b56e87e981ce18c365607b8b7628db54ed
majid asgari <majid.asgari@snapp.cab>
2025-12-06 10:40:02 +0330

    add nc example

--- example/server/posix/main.cpp
+++ example/server/posix/main.cpp
@@ -1,6 +1,10 @@
 #include "server/posix/tcp_server.h"
 #include <iostream>
 
+/*
+    connect to server by nc -> nc 127.0.0.1 8083
+*/
+
 std::string callback_function(int client_id, const std::string& req){
     std::cout << "callback Received from clientID["<<client_id<<"] msg: " << req << std::endl;
     return "Echo: " + req;  
